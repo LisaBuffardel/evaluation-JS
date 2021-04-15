c398273dda7af9ccd2ba4c09f7c20fdf003b7af1
Lisa Buffardel <lisa.buffardel@gmail.com>
2021-04-15 14:45:07 +0200

    finaliser les fonctions du jeu

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+    //**fonctions pour générer un dé en css */
     function d1() {
         console.log('d1')
         $('#d05').css('background-color', 'red')
@@ -28,53 +29,125 @@ $(document).ready(() => {
         console.log('d6')
         $('#d01, #d03, #d04, #d06, #d07, #d09').css('background-color', 'red')
     }
+    //**fin fonctions générateur de dé */
 
+    //**fonction du joueur en cours */
+    console.log('initialisation')
+    var players = false; //**false = player1 true = player2 */
+
+    //**fin fonction joueur en cours */
+
+    //**fonction de générations aléatoire du dé */
     $('#lancerD').click(function() {
-        var minNumber = 1; // le minimum
-        var maxNumber = 5; // le maximum
-        var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); // la fonction magique
-        console.log(randomnumber)
+            var minNumber = 1; // le minimum
+            var maxNumber = 5; // le maximum
+            var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); // la fonction magique
+            console.log(randomnumber)
+            console.log(players)
 
-        $('.pointD').css('background-color', 'white')
+            if (players == false) {
+                console.log('player1')
+                var scoreCurrent = $('.scoreCurrent1').text()
+            }
+            if (players == true) {
+                console.log('player2')
+                var scoreCurrent = $('.scoreCurrent2').text()
+            }
+            var score = parseInt(scoreCurrent, 10) + randomnumber
 
-        if (randomnumber === 1) {
-            d1()
-        }
-        if (randomnumber === 2) {
-            d2()
-        }
-        if (randomnumber === 3) {
-            d3()
-        }
-        if (randomnumber === 4) {
-            d4()
-        }
-        if (randomnumber === 5) {
-            d5()
-        }
-        if (randomnumber === 6) {
-            d6()
-        }
 
-        var scoreCurrent = $('.scoreCurrent1').text()
-        var score = parseInt(scoreCurrent, 10)
-        $('.scoreCurrent1').text(score + randomnumber)
-    })
+            $('.pointD').css('background-color', 'white')
 
+            if (randomnumber === 1) {
+                d1()
+                score = 0
+            }
+            if (randomnumber === 2) {
+                d2()
+            }
+            if (randomnumber === 3) {
+                d3()
+            }
+            if (randomnumber === 4) {
+                d4()
+            }
+            if (randomnumber === 5) {
+                d5()
+            }
+            if (randomnumber === 6) {
+                d6()
+            }
+
+
+            if (players == false) {
+                $('.scoreCurrent1').text(score)
+                if (score == 0) {
+                    console.log('changement joueur')
+                    players = true
+                }
+            } else if (players == true) {
+                $('.scoreCurrent2').text(score)
+                if (score == 0) {
+                    console.log('changement joueur')
+                    players = false
+                }
+            }
+
+
+        })
+        //**fin dé aléatoire */
+
+    //**redémarrer une partie */
     $('#newGame').click(function() {
-        $('.scoreCurrent1').text(0)
-        $('.globalScore1').text(0)
-    })
+            $('.scoreCurrent1').text(0)
+            $('.globalScore1').text(0)
+            $('.scoreCurrent2').text(0)
+            $('.globalScore2').text(0)
+            players = false
+        })
+        //**fin fonction redémarrer une partie */
 
+    //**fonction hold : ajouter current au global + changement de joueur*/
     $('#hold').click(function() {
-        var globalScore1 = $('.globalScore1').text()
-        var globalScore = parseInt(globalScore1, 10)
-        var scoreCurrent1 = $('.scoreCurrent1').text()
-        var scoreCurrent = parseInt(scoreCurrent1, 10)
-        $('.globalScore1').text(scoreCurrent + globalScore)
-        $('.scoreCurrent1').text(0)
-    })
+            if (players == false) {
+                var globalScore1 = $('.globalScore1').text()
+                var globalScore = parseInt(globalScore1, 10)
+                var scoreCurrent1 = $('.scoreCurrent1').text()
+                var scoreCurrent = parseInt(scoreCurrent1, 10)
+                $('.globalScore1').text(scoreCurrent + globalScore)
+                $('.scoreCurrent1').text(0)
+                winners(scoreCurrent + globalScore)
+                players = true
+            } else if (players == true) {
+                var globalScore2 = $('.globalScore2').text()
+                var globalScore = parseInt(globalScore2, 10)
+                var scoreCurrent2 = $('.scoreCurrent2').text()
+                var scoreCurrent = parseInt(scoreCurrent2, 10)
+                $('.globalScore2').text(scoreCurrent + globalScore)
+                $('.scoreCurrent2').text(0)
+                winners(scoreCurrent + globalScore)
+                players = false
+            }
 
+
+
+        })
+        //**fin fonction hold */
+
+
+    //**Fonctions pour déclarer le gagnant de la partie */
+    function winners(score) {
+        if (players == false) {
+            var name = 'player1'
+        }
+        if (players == true) {
+            var name = 'player2'
+        }
+        if (score >= 100) {
+            alert(name + ' gagne la partie')
+        }
+    }
+    //**Fin fonction winners */
 
 
 
